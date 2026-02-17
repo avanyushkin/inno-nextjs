5ac8542e479c76d0920e98ab1e5675392215f20a
avanyushkin <a.vanyushkin1@gmail.com>
2026-02-17 17:30:02 +0300

    feat (fix): rename Header file for Vercel deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
-import Header from "@/components/ui/header";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "@/providers/provider";
+import Header from "@/components/ui/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
