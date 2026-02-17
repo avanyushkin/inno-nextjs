6b1cfbdbd4bf8e2d0a19e607cbe9dfd02308186a
avanyushkin <a.vanyushkin1@gmail.com>
2026-02-17 17:37:48 +0300

    feat (main): fix letters register

--- src/app/login/login-client.tsx
+++ src/app/login/login-client.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+
 import { Button } from "@/components/ui/Button";
 import { Input } from "@/components/ui/Input";
 import { Card } from "@/components/ui/Card";
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/Button";
-import { Input } from "@/components/ui/input";
+import { Input } from "@/components/ui/Input";
 import { Card } from "@/components/ui/Card";
 import { Link } from "@heroui/react";
 
